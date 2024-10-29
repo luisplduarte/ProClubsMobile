0200b1fedcd8bf68a7f2928fd8e54595284422a0
luisplduarte <luis.pedro.duarte.24@gmail.com>
2024-10-29 15:59:45 +0000

    finished login and sign up pages

--- api/authService.ts
+++ api/authService.ts
@@ -1,13 +1,17 @@
 import { auth } from '../utils/firebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, AuthError } from 'firebase/auth';
 
 export const signUp = async (email: string, password: string) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     return userCredential.user.getIdToken();
   } catch (error) {
-    console.error("Error during sign-up:", error);
-    throw error;
+    const authError = error as AuthError;
+    let errorMessage = "An unknown error occurred. Please try again.";
+    if (authError.code == 'auth/email-already-in-use') {
+      errorMessage = "This email is already in use. Please try logging in or use another email.";
+    }
+    throw new Error(errorMessage);
   }
 };
 
