dbb200a7be146312b686ab231013eb86b6c9fab4
luisplduarte <luis.pedro.duarte.24@gmail.com>
2024-10-30 16:33:08 +0000

    Added circular graph to club info screen

--- types/ClubInfoTypes.ts
+++ types/ClubInfoTypes.ts
@@ -1,7 +1,23 @@
+// TODO: return fields that are of time numbers as numbers from the API and then change the types here also
+
 export interface ClubInfo {
-    clubId: string;
+    id: string;
     name: string;
-    regionId: string;
+    regionId: number;
+    teamId: number;
+    bestDivision: string;
+    gamesPlayed: string;
+    leagueAppearances: string;
+    gamesPlayedPlayoff: string;
+    goals: string;
+    goalsAgainst: string;
+    promotions: string;
+    relegations: string;
+    wins: string;
+    losses: string;
+    ties: string;
+    winStreak: string;
+    skillRating: string;
   }
   
   export interface ApiResponse {
