a896009269de912ad3f4770e9d4b0e2cc25a55f9
luisplduarte <luis.pedro.duarte.24@gmail.com>
2024-10-30 15:00:15 +0000

    Small change on login and signup inputs with

--- utils/axiosConfig.ts
+++ utils/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://proclubs.ea.com/api/fc', Pro clubs API base URL
-  baseURL: 'https://cb7b-2001-818-dcaa-f200-c02c-d6d7-98ca-c28c.ngrok-free.app/api', // Ngrok url to API
+  baseURL: 'https://b83e-2001-818-dcaa-f200-5c2e-3b46-ccfd-249b.ngrok-free.app/api', // Ngrok url to API
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
