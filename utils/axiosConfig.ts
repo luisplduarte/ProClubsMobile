fd4bec4634348c4ba9ae8952dae715a1457012be
luisplduarte <luis.pedro.duarte.24@gmail.com>
2024-11-07 12:02:55 +0000

    .

--- utils/axiosConfig.ts
+++ utils/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://proclubs.ea.com/api/fc', Pro clubs API base URL
-  baseURL: 'https://9418-2001-818-dcaa-f200-adb3-7f4-f3a-f5d0.ngrok-free.app/api', // Ngrok url to API
+  baseURL: `https://3a14-2001-818-dcaa-f200-714d-7990-92d0-7392.ngrok-free.app/api`, // Ngrok url to API
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
