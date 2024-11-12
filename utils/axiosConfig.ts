d18fcb3f6f06797202db18ad4a93814fd743254c
luisplduarte <luis.pedro.duarte.24@gmail.com>
2024-11-12 09:24:42 +0000

    Added toast when connectivity state changes

--- utils/axiosConfig.ts
+++ utils/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://proclubs.ea.com/api/fc', Pro clubs API base URL
-  baseURL: `https://3a14-2001-818-dcaa-f200-714d-7990-92d0-7392.ngrok-free.app/api`, // Ngrok url to API
+  baseURL: `https://f926-2001-818-dcaa-f200-917e-8078-3d8b-923a.ngrok-free.app/api`, // Ngrok url to API
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
