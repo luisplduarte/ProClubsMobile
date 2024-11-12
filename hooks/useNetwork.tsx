2850ac70fbccfb672d534456177e580fb71d9e8f
luisplduarte <luis.pedro.duarte.24@gmail.com>
2024-11-12 09:50:46 +0000

    useNetwork hook finished

--- hooks/useNetwork.tsx
+++ hooks/useNetwork.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import NetInfo from "@react-native-community/netinfo";
 import { onlineManager } from "@tanstack/react-query";
 import Toast from 'react-native-toast-message';
@@ -8,39 +8,36 @@ export const useNetwork = () => {
   const hasCheckedInitialConnection = useRef<boolean>(false);
   const lastConnectionState = useRef<boolean | null>(null); // Track the previous connection state to detect changes
 
-  const handleMessage = useCallback((isConnected: boolean) => {
+  const handleMessage = (isConnected: boolean) => {
     onlineManager.setOnline(isConnected);
-    Toast.show({
-      type: isConnected ? 'success' : 'error',
-      text1: isConnected ? 'Connection Restored' : 'Connection Lost',
-      text2: isConnected ? 'You are back online!' : 'You have lost your internet connection.',
-      position: 'top',
-    });
-  }, []);
 
-  const checkInitialConnection = useCallback(async () => {
-    const state = await NetInfo.fetch();
-    const isConnected = !!state.isConnected;
-
-    hasCheckedInitialConnection.current = true;
-    lastConnectionState.current = isConnected;
-
-    if (!isConnected) {
-      handleMessage(false);
+    if (hasCheckedInitialConnection.current) {
+      Toast.show({
+        type: isConnected ? 'success' : 'error',
+        text1: isConnected ? 'Connection Restored' : 'Connection Lost',
+        text2: isConnected ? 'You are back online!' : 'You have lost your internet connection.',
+        position: 'top',
+      });
     }
-  }, [handleMessage]);
+  };
 
   useEffect(() => {
-    checkInitialConnection();
-
-    const unsubscribe = NetInfo.addEventListener((state) => {
+    // First initial verification
+    NetInfo.fetch().then(state => {
       const isConnected = !!state.isConnected;
-      if ( hasCheckedInitialConnection.current && lastConnectionState.current !== isConnected ) {
+      lastConnectionState.current = isConnected;
+      hasCheckedInitialConnection.current = true; // Checks that first verification was done
+    });
+
+    const unsubscribe = NetInfo.addEventListener(state => {
+      const isConnected = !!state.isConnected;
+
+      if (hasCheckedInitialConnection.current && lastConnectionState.current !== isConnected) {
         handleMessage(isConnected);
         lastConnectionState.current = isConnected;
       }
     });
-    return () => unsubscribe();
-  }, [checkInitialConnection, handleMessage]);
 
-}
+    return () => unsubscribe();
+  }, []);
+};
